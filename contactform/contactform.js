5789c91acac70ecab674fdb8673020a14344d266
Arman R <armanr123@gmail.com>
2019-10-07 17:03:15 -0700

    small fix

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -8,6 +8,7 @@ jQuery(document).ready(function($) {
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
     f.children('input').each(function() { // run all inputs
+
       var i = $(this); // current input
       var rule = i.attr('data-rule');
 
