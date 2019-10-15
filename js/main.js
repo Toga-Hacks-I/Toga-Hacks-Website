24a4988b74f7503119d4aecdeb78cabe8bf6cb18
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-10-15 14:47:50 -0700

    Add Cloudinary library

--- js/main.js
+++ js/main.js
@@ -30,6 +30,9 @@ import "venobox/venobox/venobox";
 // import owl carousel
 import "owl.carousel2/dist/owl.carousel";
 
+// import cloudinary
+import "cloudinary-jquery/cloudinary-jquery";
+
 $(document).ready(function() {
   // Back to top button
   $(window).scroll(() => {
