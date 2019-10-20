3cf98a4678f5b4aa443bb4373659f4ac3a958e70
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2019-10-20 11:53:09 -0700

    Implement PurgeCSS to remove unused CSS (#13)

---
+++ purgecss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html"],
+  css: ["./css/style.css"],
+  whitelist: [
+    "header-fixed",
+    "mobile-nav-active",
+    "menu-item-active",
+    "fa-chevron-down",
+    "fa-chevron-down",
+    "menu-has-children",
+    "mobile-body-overly",
+    "fa-bars",
+    "mobile-nav",
+    "mobile-nav-toggle",
+    "header-scrolled",
+    "header-fixed"
+  ]
+};
