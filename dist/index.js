f6ca1dc317f0df95f4537340d69bf6fe600ca114
rvignav <rvignav@gmail.com>
2020-01-16 19:19:07 -0800

    final UI version

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55284" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55641" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
