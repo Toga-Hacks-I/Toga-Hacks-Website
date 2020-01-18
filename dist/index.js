082939d45f1e31b18605c8f066a8aa708900faa5
rvignav <rvignav@gmail.com>
2020-01-17 21:49:23 -0800

    added media queries

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57358" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60933" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
