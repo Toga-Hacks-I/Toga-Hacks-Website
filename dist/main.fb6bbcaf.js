7074a4b572d9b1b948ec845640ebce462688e1f5
rvignav <rvignav@gmail.com>
2020-01-17 22:40:19 -0800

    final event schedule UI

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -33315,7 +33315,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57358" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61881" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
