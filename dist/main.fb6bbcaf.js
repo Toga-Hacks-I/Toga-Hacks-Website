2a9856633eacdd1bd5ca19c6568fe1d85d754df3
masonwang025 <mason.wang0025@gmail.com>
2020-03-20 09:59:43 -0700

    page design slants less dramatic

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -33607,7 +33607,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61612" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63881" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
