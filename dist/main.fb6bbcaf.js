aebcaf50b9b7029d1b5334996103382804bd978d
rvignav <rvignav@gmail.com>
2021-03-19 21:04:12 -0700

    Update Register button

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -33315,7 +33315,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56031" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65534" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
